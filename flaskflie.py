0ff7e7d53776bd4bd41b552cc0116d81066d445d
Sahil Sambhaji Bodkhe <79392045+SAHILBODKHE@users.noreply.github.com>
2023-04-23 13:32:24 +0530

    update new error solved photo 1 and 2 saved

--- flaskflie.py
+++ flaskflie.py
@@ -1,49 +1,54 @@
-from flask import Flask, request
+from flask import Flask, request,jsonify
 import face_recognition
 import cv2
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/upload-photo', methods=['POST'])
 def upload_photo():
-    photo = request.files.get('photo')
-    if not photo:
-        return 'No photo provided', 400
+    if 'photo' not in request.files:
+        return jsonify({'error': 'No photo provided'}), 400
+    else:
+        photo1 = request.files.get('photo')
+        photo2=request.files.get('photo2')
+        photo1.save('phot1.jpg')
+        photo2.save('phot2.jpg')
+    
 
-    # Save photo to disk (optional)
-    photo[0].save('phot1.jpg')
-    photo[1].save('phot2.jpg')
-   
 
-#Load the images containing faces
-    image1 = face_recognition.load_image_file("./phot2.jpg")
-    image2 = face_recognition.load_image_file("./phot1.jpg")
+        image1 = face_recognition.load_image_file("./phot2.jpg")
+        image2 = face_recognition.load_image_file("./phot1.jpg")
 
-    # #Locate the faces in the images
-    face_locations1 = face_recognition.face_locations(image1)
-    face_locations2 = face_recognition.face_locations(image2)
+        # image1 = face_recognition.load_image_file("./image1.jpg")
+        # image2 = face_recognition.load_image_file("./image.jpg")
+        # image2 = face_recognition.load_image_file("./image2.png")
 
-    # #Encode the faces
-    face_encodings1 = face_recognition.face_encodings(image1, face_locations1)
-    face_encodings2 = face_recognition.face_encodings(image2, face_locations2)
+        #Locate the faces in the images
+        face_locations1 = face_recognition.face_locations(image1)
+        face_locations2 = face_recognition.face_locations(image2)
 
-    # #Compare the face encodings using a distance metric
-    match_found = False
-    for face_encoding1 in face_encodings1:
-        for face_encoding2 in face_encodings2:
-            results = face_recognition.compare_faces([face_encoding1], face_encoding2)
-            if results[0] == True:
-                match_found = True
-                return("Match found!",200)
+        #Encode the faces
+        face_encodings1 = face_recognition.face_encodings(image1, face_locations1)
+        face_encodings2 = face_recognition.face_encodings(image2, face_locations2)
+
+        #Compare the face encodings using a distance metric
+        match_found = False
+        for face_encoding1 in face_encodings1:
+            for face_encoding2 in face_encodings2:
+                results = face_recognition.compare_faces([face_encoding1], face_encoding2)
+                if results[0] == True:
+                    match_found = True
+                    return("Match found!",200)
+                    break
+            if match_found:
                 break
-        if match_found:
-            break
-    if not match_found:
-        return("Match not found",400)
+        if not match_found:
+            return("Match not found",400)
 
     # Process the photo (optional)
     # ...
 
-    # return 'Photo received', 200
+  
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=443)
